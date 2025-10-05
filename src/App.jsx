f5f5785bac6d230e43d02d8aa5040225e478bc05
EL Mehdi El Adnani <eladnanimehdi@gmail.com>
2025-10-05 20:07:07 +0100

    modification cv

--- src/App.jsx
+++ src/App.jsx
@@ -149,20 +149,24 @@ const handleSubmit = (e) => {
               className=" mb-6"
             />
             <div className="flex items-center sm:gap-4 gap-2">
-              <a 
-                href="./assets/CV.pdf" 
-                download="ElAdnani_ELMEhdi_CV.pdf" 
-                className="font-semibold bg-[#1a1a1a] p-4 px-6 rounded-full border border-gray-700 hover:bg-[#222] transition-colors"
-              >
-                <ShinyText text="Download CV (English)" disabled={false} speed={3} className="custom-class" />
-              </a>
-                            <a 
-                href="./assets/cv_fr.pdf" 
-                download="ElAdnani_ELMEhdi_CV.pdf" 
-                className="font-semibold bg-[#1a1a1a] p-4 px-6 rounded-full border border-gray-700 hover:bg-[#222] transition-colors"
-              >
-                <ShinyText text="Télécharger CV (Français)" disabled={false} speed={3} className="custom-class" />
-              </a>
+                <a
+                  href="/assets/CV.pdf"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="font-semibold bg-[#1a1a1a] p-4 px-6 rounded-full border border-gray-700 hover:bg-[#222] transition-colors"
+                >
+                  <ShinyText text="Download CV (English)" disabled={false} speed={3} className="custom-class" />
+                </a>
+
+                  <a
+                    href="/assets/cv_fr.pdf"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="font-semibold bg-[#1a1a1a] p-4 px-6 rounded-full border border-gray-700 hover:bg-[#222] transition-colors"
+                  >
+                    <ShinyText text="Télécharger CV (Français)" disabled={false} speed={3} className="custom-class" />
+                  </a>
+
 
               <a href="#project" className="font-semibold bg-[#1a1a1a] p-4 px-6 rounded-full border border-gray-700 hover:bg-[#222] transition-colors">
                 <ShinyText text="Explore My Projects" disabled={false} speed={3} className="custom-class" />
