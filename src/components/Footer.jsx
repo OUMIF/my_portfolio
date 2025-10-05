473399600da29ae6f8e382d7e161ff7039f9a00e
EL Mehdi El Adnani <eladnanimehdi@gmail.com>
2025-10-05 16:39:56 +0100

    finit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -21,9 +21,17 @@ const Footer = () => {
 
         {/* Ikon Sosmed - di tengah di mobile */}
         <div className="flex gap-3 order-2 md:order-none">
-          <a href="https://github.com/rissss21"><i className="ri-github-fill ri-2x"></i></a>
-          <a href="https://www.instagram.com/farisedrikprayoga/"><i className="ri-instagram-fill ri-2x"></i></a>
-          <a href="https://www.youtube.com/@FarisEdrikPrayoga"><i className="ri-youtube-fill ri-2x"></i></a>
+          <a href="https://github.com/OUMIF" target="_blank" rel="noopener noreferrer">
+            <i className="ri-github-fill ri-2x"></i>
+          </a>
+          
+          <a href="https://www.linkedin.com/in/el-mehdi-el-adnani-a4566425a/" target="_blank" rel="noopener noreferrer">
+            <i className="ri-linkedin-box-fill ri-2x"></i>
+          </a>
+          
+          <a href="https://leetcode.com/u/oumif/" target="_blank" rel="noopener noreferrer">
+            <i className="ri-code-box-fill ri-2x"></i>
+          </a>
         </div>
 
         {/* Dock - paling bawah di mobile */}
